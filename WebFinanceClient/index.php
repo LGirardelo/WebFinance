63518646c7534aaa5a147b8e26f5d15d83ed4b43
Leandro Girardelo <leandrogirardelo@gmail.com>
2019-02-06 23:39:12 -0200

    Ajustes icons

--- WebFinanceClient/index.php
+++ WebFinanceClient/index.php
@@ -74,7 +74,7 @@
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="dist/img/avatar2.png" class="user-image" alt="User Image">
+              <img src="dist/img/emoji_fin.png" class="user-image" alt="User Image">
               <span class="hidden-xs"> <?php echo $_SESSION['name'] ?> </span>
             </a>
             <ul class="dropdown-menu">
@@ -113,7 +113,7 @@
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="dist/img/avatar2.png" class="img-circle" alt="User Image">
+          <img src="dist/img/emoji_fin.png" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           <p><?php echo $_SESSION['name'] ?></p>
